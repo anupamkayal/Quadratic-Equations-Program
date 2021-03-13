255a4c9ef7fdfa973dea62e30c676585b83e0769
anupamkayal <73344006+anupamkayal@users.noreply.github.com>
2021-03-13 21:19:47 +0530

    Add files via upload

---
+++ quadratic equations.py
@@ -0,0 +1,22 @@
+#equa= (input('please enter equation :  '))
+a= int(input('please enter the value of a:  '))
+b= int(input('please enter the value of b:  '))
+c=int( input('please enter the value of c:  '))
+import math
+
+def quadratric(a,b,c,):
+	d=math.sqrt((b**2-4*a*c))
+	x=(-(b)+d)/(2*a)
+	print('+x='+str(x))
+	d=math.sqrt((b**2-4*a*c))
+	x=(-(b)-d)/(2*a)
+	print('-x='+str(x))
+	
+quadratric(a,b,c,)
+	
+	
+	
+	
+
+
+
